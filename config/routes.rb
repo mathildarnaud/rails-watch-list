0bc7a088761efd43eec4503125ba43551c7c7730
Mathilde Arnaud <mathildarnaud0598@gmail.com>
2022-11-17 16:47:39 +0100

    first watch list step

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index new show create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
